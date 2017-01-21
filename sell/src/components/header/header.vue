51df74770ae12c9e108922b8e9b63fede5d05dfa
zhiliang <314339793@qq.com>
2017-01-21 22:24:02 +0800

    add details index

--- sell/src/components/header/header.vue
+++ sell/src/components/header/header.vue
@@ -17,18 +17,21 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
-      <div v-if="seller.supports" class="support-count">
+      <div v-if="seller.supports" class="support-count" v-on:click="showDetail">
         <span class="count">{{seller.supports.length}}个</span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
-    <div class="bulletin-wrapper">
+    <div class="bulletin-wrapper" v-on:click="showDetail">
       <span class="bulletin-title"></span>
       <span class="bulletin-text">{{seller.bulletin}}</span>
       <i class="icon-keyboard_arrow_right"></i>
     </div>
     <div class="background">
       <img width="100%" height="100%" v-bind:src="seller.avatar">
+    </div>
+    <div class="detail" v-show="detailShow">
+
     </div>
   </div>
 </template>
@@ -37,6 +40,16 @@
     props: {
       seller: {
         type: Object
+      }
+    },
+    data() {
+      return {
+        detailShow: false
+      }
+    },
+    methods: {
+      showDetail() {
+        this.detailShow = true;
       }
     },
     created() {
@@ -50,6 +63,7 @@
     position: relative
     color: #fff
     background: rgba(7,17,27,0.4)
+    overflow: hidden
     .content-wrapper
       position: relative
       font-size: 0
@@ -135,7 +149,7 @@
       .bulletin-title
         display: inline-block
         vertical-align: top
-        margin-top: 7px
+        margin-top: 8px
         width: 22px
         height: 12px
         bg-image('./bulletin')
@@ -158,4 +172,13 @@
       height: 100%
       z-index: -1
       filter: blur(10px)
+    .detail
+      position: fixed
+      top: 0
+      left: 0
+      z-index: 100
+      width: 100%
+      height: 100%
+      overflow: auto
+      background: rgba(7,17,27,0.8)
 </style>
